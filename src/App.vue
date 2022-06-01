4acd4ea334e326a6179eed383f2a801a5f6f8178
drunkenhw <drunkenhw@gmail.com>
2022-06-01 11:36:45 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Exercise msg="Welcome to Your Vue.js App"/>
+  <Exercise2 msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+// import Exercise from './components/Vue01_data.vue'
+// import Exercise from './components/Vue02_declaritive_rendering.vue'
+// import Exercise from './components/Vue05_vshow.vue'
+import Exercise from './components/Vue07_vfor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Exercise
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
